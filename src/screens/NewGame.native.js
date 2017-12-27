2077abc68e82da3def594bb564b619b42a78051b
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-12-27 22:34:10 +0300

    new game base added.

--- src/screens/NewGame.native.js
+++ src/screens/NewGame.native.js
@@ -3,18 +3,52 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
+    Text,
+    TouchableOpacity,
 } from 'react-native';
 
 export default class NewGame extends Component {
     render() {
         return (
-            <View />
+            <View
+                style={styles.body}>
+                <TouchableOpacity
+                    style={[styles.shape, styles.shape1]}>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.shape, styles.shape2]}>
+
+                </TouchableOpacity>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
+    body: {
+        flex: 1,
+    },
+    shape: {
+        // flex: 1,
+        position: 'absolute',
+        transform: [{
+            rotate: '45deg'
+        }]
+    },
+    shape1: {
+        width: '100%',
+        height: 700,
+        left: -115,
+        top: -200,
+        backgroundColor: 'blue',
+    },
+    shape2: {
+        width: '100%',
+        height: 700,
+        right: -115,
+        bottom: -200,
+        backgroundColor: 'red',
+    },
 });
 
 NewGame.navigationOptions = {
