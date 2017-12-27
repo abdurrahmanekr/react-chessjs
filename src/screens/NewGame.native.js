a9d8a671cbd92f7fd1afcbfe83ae681b273159be
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-12-27 21:57:54 +0300

    New Game component added.

---
+++ src/screens/NewGame.native.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import {
+    StyleSheet,
+    View,
+} from 'react-native';
+
+export default class NewGame extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+NewGame.navigationOptions = {
+    title: 'New Game'
+}
