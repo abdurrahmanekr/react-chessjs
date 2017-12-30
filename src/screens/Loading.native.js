d0b429c11a1d29d3332a8d634144bf435202158c
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2017-12-30 20:51:55 +0300

    login design edited.

--- src/screens/Loading.native.js
+++ src/screens/Loading.native.js
@@ -4,20 +4,142 @@ import {
     StyleSheet,
     View,
     Text,
+    TouchableOpacity,
 } from 'react-native';
 
 export default class Loading extends Component {
     render() {
         return (
-            <View>
-                <Text>Loading</Text>
+            <View
+                style={styles.body}>
+                <View
+                    style={styles.topContent}>
+                    <View
+                        style={styles.user}>
+                        <View
+                            style={styles.userPhoto}>
+                        </View>
+                        <View
+                            style={styles.userName}>
+                            <Text
+                                style={styles.userNameText}>
+                                Jhorge
+                            </Text>
+                        </View>
+                    </View>
+                    <View
+                        style={styles.user}>
+                        <View
+                            style={styles.userPhoto}>
+                        </View>
+                        <View
+                            style={styles.userName}>
+                            <Text
+                                style={styles.userNameText}>
+                                Izabella
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View
+                    style={styles.bottomContent}>
+                    <View
+                        style={styles.buttons}>
+                        <View
+                            style={styles.button}>
+                            <TouchableOpacity
+                                style={[styles.buttonBody, { backgroundColor: '#3aaaa2' }]}>
+                                <Text
+                                    style={styles.buttonText}>
+                                    1 Minute
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View
+                        style={styles.buttons}>
+                        <View
+                            style={[styles.button, styles.playButton]}>
+                            <TouchableOpacity
+                                style={[styles.playButtonBody, styles.buttonBody, { backgroundColor: '#44dd66' }]}>
+                                <Text
+                                    style={styles.buttonText}>
+                                    Play
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    // top content
+    topContent: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    user: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    userPhoto: {
+        width: 80,
+        height: 80,
+        borderRadius: 25,
+        backgroundColor: 'red',
+    },
+    userName: {
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    userNameText: {
+        fontSize: 17,
+        color: '#333',
+    },
+    // bottom content
+    bottomContent: {
+        flex: 2,
+        backgroundColor: '#efefef',
+    },
+    buttons: {
+        flexDirection: 'row',
+    },
+    button: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 20,
+        paddingBottom: 20,
+    },
+    buttonBody: {
+        padding: 20,
+        paddingLeft: 30,
+        paddingRight: 30,
+        borderRadius: 5,
+        backgroundColor: '#3aaaa2',
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
+    playButton: {
+        flex: 1,
+    },
+    playButtonBody: {
+        width: '90%',
+        alignItems: 'center',
+    },
+    playButtonText: {
 
+    },
 });
 
 Loading.navigationOptions = {
